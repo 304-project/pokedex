ce8fda552e78c41ef4e271edcfcffed8f19843e7
vincent-tur <vincentturato@gmail.com>
2018-03-25 21:55:41 -0700

    converting users route to typescript

--- routes/users.ts
+++ routes/users.ts
@@ -1,45 +1,51 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var app_1 = require("../app");
+import Main from '../app';
+import * as express from "express"
+
 // SHOW LIST OF USERS
-var UsersRoute = /** @class */ (function () {
-    function UsersRoute() {
-    }
-    UsersRoute.showUsers = function (req, res) {
-        var sql = 'SELECT * FROM users ORDER BY id DESC';
-        app_1.default.connection.query(sql, function (err, rows, fields) {
+
+export class UsersRoute {
+
+    public static showUsers(req, res: express.Response) {
+        let sql: string = 'SELECT * FROM users ORDER BY id DESC';
+        Main.connection.query(sql, (err, rows, fields) => {
             //if(err) throw err
             if (err) {
-                req.flash('error', err);
+                req.flash('error', err)
                 res.render('user/list', {
                     title: 'User List',
                     data: ''
-                });
-            }
-            else {
+                })
+            } else {
                 // render to views/user/list.ejs template file
                 res.render('user/list', {
                     title: 'User List',
                     data: rows
-                });
+                })
             }
         });
-    };
-    UsersRoute.showAddUserForm = function (req, res) {
+    }
+
+
+
+    public static showAddUserForm(req, res: express.Response) {
         res.render('user/add', {
             title: 'Add New User',
             name: '',
             age: '',
             email: ''
         });
-    };
+    }
+
     // ADD NEW USER POST ACTION
-    UsersRoute.addNewUser = function (req, res) {
-        req.assert('name', 'Name is required').notEmpty(); //Validate name
-        req.assert('age', 'Age is required').notEmpty(); //Validate age
-        req.assert('email', 'A valid email is required').isEmail(); //Validate email
-        var errors = req.validationErrors();
-        if (!errors) {
+    public static addNewUser(req, res: express.Response) {
+        req.assert('name', 'Name is required').notEmpty()           //Validate name
+        req.assert('age', 'Age is required').notEmpty()             //Validate age
+        req.assert('email', 'A valid email is required').isEmail()  //Validate email
+
+        var errors = req.validationErrors()
+
+        if (!errors) {   //No errors were found.  Passed Validation!
+
             /********************************************
              * Express-validator module
 
@@ -53,39 +59,42 @@ var UsersRoute = /** @class */ (function () {
                 name: req.sanitize('name').escape().trim(),
                 age: req.sanitize('age').escape().trim(),
                 email: req.sanitize('email').escape().trim()
-            };
+            }
+
             req.getConnection(function (error, conn) {
                 conn.query('INSERT INTO users SET ?', user, function (err, result) {
                     //if(err) throw err
                     if (err) {
-                        req.flash('error', err);
+                        req.flash('error', err)
+
                         // render to views/user/add.ejs
                         res.render('user/add', {
                             title: 'Add New User',
                             name: user.name,
                             age: user.age,
                             email: user.email
-                        });
-                    }
-                    else {
-                        req.flash('success', 'Data added successfully!');
+                        })
+                    } else {
+                        req.flash('success', 'Data added successfully!')
+
                         // render to views/user/add.ejs
                         res.render('user/add', {
                             title: 'Add New User',
                             name: '',
                             age: '',
                             email: ''
-                        });
+                        })
                     }
-                });
-            });
+                })
+            })
         }
-        else {
-            var error_msg = '';
+        else {   //Display errors to user
+            var error_msg = ''
             errors.forEach(function (error) {
-                error_msg += error.msg + '<br>';
-            });
-            req.flash('error', error_msg);
+                error_msg += error.msg + '<br>'
+            })
+            req.flash('error', error_msg)
+
             /**
              * Using req.body.name
              * because req.param('name') is deprecated
@@ -95,19 +104,21 @@ var UsersRoute = /** @class */ (function () {
                 name: req.body.name,
                 age: req.body.age,
                 email: req.body.email
-            });
+            })
         }
-    };
-    UsersRoute.showEditUserForm = function (req, res) {
-        app_1.default.connection.query('SELECT * FROM users WHERE id = ' + req.params.id, function (err, rows, fields) {
-            if (err)
-                throw err;
+    }
+
+
+    public static showEditUserForm(req, res: express.Response) {
+        Main.connection.query('SELECT * FROM users WHERE id = ' + req.params.id, function (err, rows, fields) {
+            if (err) throw err
+
             // if user not found
             if (rows.length <= 0) {
-                req.flash('error', 'User not found with id = ' + req.params.id);
-                res.redirect('/users');
+                req.flash('error', 'User not found with id = ' + req.params.id)
+                res.redirect('/users')
             }
-            else {
+            else { // if user found
                 // render to views/user/edit.ejs template file
                 res.render('user/edit', {
                     title: 'Edit User',
@@ -116,16 +127,20 @@ var UsersRoute = /** @class */ (function () {
                     name: rows[0].name,
                     age: rows[0].age,
                     email: rows[0].email
-                });
+                })
             }
-        });
-    };
-    UsersRoute.editUser = function (req, res) {
-        req.assert('name', 'Name is required').notEmpty(); //Validate name
-        req.assert('age', 'Age is required').notEmpty(); //Validate age
-        req.assert('email', 'A valid email is required').isEmail(); //Validate email
-        var errors = req.validationErrors();
-        if (!errors) {
+        })
+    }
+
+    public static editUser(req, res: express.Response) {
+        req.assert('name', 'Name is required').notEmpty()           //Validate name
+        req.assert('age', 'Age is required').notEmpty()             //Validate age
+        req.assert('email', 'A valid email is required').isEmail()  //Validate email
+
+        var errors = req.validationErrors()
+
+        if (!errors) {   //No errors were found.  Passed Validation!
+
             /********************************************
              * Express-validator module
 
@@ -140,10 +155,11 @@ var UsersRoute = /** @class */ (function () {
                 age: req.sanitize('age').escape().trim(),
                 email: req.sanitize('email').escape().trim()
             };
-            app_1.default.connection.query('UPDATE users SET ? WHERE id = ' + req.params.id, user, function (err, result) {
+            Main.connection.query('UPDATE users SET ? WHERE id = ' + req.params.id, user, function (err, result) {
                 //if(err) throw err
                 if (err) {
-                    req.flash('error', err);
+                    req.flash('error', err)
+
                     // render to views/user/add.ejs
                     res.render('user/edit', {
                         title: 'Edit User',
@@ -151,10 +167,10 @@ var UsersRoute = /** @class */ (function () {
                         name: req.body.name,
                         age: req.body.age,
                         email: req.body.email
-                    });
-                }
-                else {
-                    req.flash('success', 'Data updated successfully!');
+                    })
+                } else {
+                    req.flash('success', 'Data updated successfully!')
+
                     // render to views/user/add.ejs
                     res.render('user/edit', {
                         title: 'Edit User',
@@ -162,16 +178,16 @@ var UsersRoute = /** @class */ (function () {
                         name: req.body.name,
                         age: req.body.age,
                         email: req.body.email
-                    });
+                    })
                 }
-            });
-        }
-        else {
-            var error_msg = '';
+            })
+        } else {   //Display errors to user
+            var error_msg = ''
             errors.forEach(function (error) {
-                error_msg += error.msg + '<br>';
-            });
-            req.flash('error', error_msg);
+                error_msg += error.msg + '<br>'
+            })
+            req.flash('error', error_msg)
+
             /**
              * Using req.body.name
              * because req.param('name') is deprecated
@@ -182,25 +198,24 @@ var UsersRoute = /** @class */ (function () {
                 name: req.body.name,
                 age: req.body.age,
                 email: req.body.email
-            });
+            })
         }
-    };
-    UsersRoute.deleteUser = function (req, res) {
-        var user = { id: req.params.id };
-        app_1.default.connection.query('DELETE FROM users WHERE id = ' + req.params.id, user, function (err, result) {
+    }
+
+    public static deleteUser(req, res: express.Response) {
+        var user = {id: req.params.id};
+        Main.connection.query('DELETE FROM users WHERE id = ' + req.params.id, user, function (err, result) {
             //if(err) throw err
             if (err) {
-                req.flash('error', err);
+                req.flash('error', err)
                 // redirect to users list page
-                res.redirect('/users');
-            }
-            else {
-                req.flash('success', 'User deleted successfully! id = ' + req.params.id);
+                res.redirect('/users')
+            } else {
+                req.flash('success', 'User deleted successfully! id = ' + req.params.id)
                 // redirect to users list page
-                res.redirect('/users');
+                res.redirect('/users')
             }
         });
-    };
-    return UsersRoute;
-}());
-exports.UsersRoute = UsersRoute;
+    }
+
+}
