b0c391727fdad1ac7349090aaf4db24dc5e7dbb9
Timan <timanjenga@gmail.com>
2018-03-28 11:13:16 -0700

    updated pokemon form

--- routes/pokemon.js
+++ routes/pokemon.js
@@ -26,7 +26,93 @@ var PokemonRoute = (function () {
     };
     PokemonRoute.search = function (req, res) {
     };
+    PokemonRoute.showEvaluatePokemonForm = function (req, res) {
+        console.log("at least we get here");
+        console.log("at least we get here");
+        console.log("at least we get here");
+        console.log("at least we get here");
+        console.log("at least we get here");
+        console.log("at least we get here");
+        res.render('pokemon/evaluate', {
+            title: 'Pokemon Evaluate',
+            filterName: '',
+            filterType: '',
+            filterHabitat: '',
+            groupType: '',
+            groupHabitat: '',
+            groupRegion: '',
+            sortId: '',
+            sortHeight: '',
+            sortWeight: ''
+        });
+    };
+    PokemonRoute.filterPokemon = function (req, res) {
+        var temp = null;
+        var filterQuery = 'SELECT * FROM pokemon WHERE ';
+        app_1.default.connection.query(query, function (err, rows, fields) {
+            if (err) {
+                req.flash('error', err);
+            }
+            else {
+                app_1.default.connection.query(query, function (err, rows, fields) {
+                    if (err) {
+                        req.flash('error', err);
+                        res.render('pokemon/list', {
+                            title: 'Pokemon List',
+                            data: '',
+                            loggedInUser: app_1.default.loggedInUser.getJson()
+                        });
+                    }
+                    else {
+                        res.render('pokemon/list', {
+                            title: 'Pokemon List',
+                            data: rows,
+                            loggedInUser: app_1.default.loggedInUser.getJson()
+                        });
+                    }
+                });
+            }
+        });
+    };
+    PokemonRoute.sortPokemon = function (req, res) {
+        app_1.default.connection.query(query, function (err, rows, fields) {
+            if (err) {
+                req.flash('error', err);
+                res.render('pokemon/list', {
+                    title: 'Pokemon List',
+                    data: '',
+                    loggedInUser: app_1.default.loggedInUser.getJson()
+                });
+            }
+            else {
+                res.render('pokemon/list', {
+                    title: 'Pokemon List',
+                    data: rows,
+                    loggedInUser: app_1.default.loggedInUser.getJson()
+                });
+            }
+        });
+    };
+    PokemonRoute.groupPokemon = function (req, res) {
+        app_1.default.connection.query(query, function (err, rows, fields) {
+            if (err) {
+                req.flash('error', err);
+                res.render('pokemon/list', {
+                    title: 'Pokemon List',
+                    data: '',
+                    loggedInUser: app_1.default.loggedInUser.getJson()
+                });
+            }
+            else {
+                res.render('pokemon/list', {
+                    title: 'Pokemon List',
+                    data: rows,
+                    loggedInUser: app_1.default.loggedInUser.getJson()
+                });
+            }
+        });
+    };
     return PokemonRoute;
 }());
 exports.PokemonRoute = PokemonRoute;
-//# sourceMappingURL=pokemon.js.map
+//# sourceMappingURL=pokemons.js.map
--- routes/pokemon.ts
+++ routes/pokemon.ts
@@ -29,4 +29,108 @@ export class PokemonRoute {
         
     }
 
+    public static showEvaluatePokemonForm(req: any, res: express.Response) {
+
+        console .log ("at least we get here");
+        console .log ("at least we get here");
+        console .log ("at least we get here");
+        console .log ("at least we get here");
+        console .log ("at least we get here");
+        console .log ("at least we get here");
+
+
+        res.render('pokemon/evaluate', {
+            title: 'Pokemon Evaluate',
+            filterName: '',
+            filterType: '',
+            filterHabitat: '',
+            groupType: '',
+            groupHabitat: '',
+            groupRegion: '',
+            sortId: '',
+            sortHeight:'',
+            sortWeight:''
+
+        });
+    }
+
+    public static filterPokemon(req: any, res: express.Response) {
+
+        let temp:any = null ;
+        let filterQuery = 'SELECT * FROM pokemon WHERE ';
+
+
+        Main.connection.query(query, (err: any, rows: any, fields: any) => {
+            if (err) {
+                req.flash('error', err);
+
+            } else {
+                Main.connection.query(query, (err: any, rows: any, fields: any) => {
+                    if (err) {
+                        req.flash('error', err);
+                        res.render('pokemon/list', {
+                            title: 'Pokemon List',
+                            data: '',
+                            loggedInUser: Main.loggedInUser.getJson()
+                        });
+                    } else {
+
+                        res.render('pokemon/list', {
+                            title: 'Pokemon List',
+                            data: rows,
+                            loggedInUser: Main.loggedInUser.getJson()
+                        });
+
+                    }
+                });
+            }
+        });
+
+
+    }
+
+    public static sortPokemon(req: any, res: express.Response) {
+
+        Main.connection.query(query, (err: any, rows: any, fields: any) => {
+            if (err) {
+                req.flash('error', err);
+                res.render('pokemon/list', {
+                    title: 'Pokemon List',
+                    data: '',
+                    loggedInUser: Main.loggedInUser.getJson()
+                });
+            } else {
+                res.render('pokemon/list', {
+                    title: 'Pokemon List',
+                    data: rows,
+                    loggedInUser: Main.loggedInUser.getJson()
+                });
+            }
+        });
+
+
+    }
+
+    public static groupPokemon(req: any, res: express.Response) {
+
+        Main.connection.query(query, (err: any, rows: any, fields: any) => {
+            if (err) {
+                req.flash('error', err);
+                res.render('pokemon/list', {
+                    title: 'Pokemon List',
+                    data: '',
+                    loggedInUser: Main.loggedInUser.getJson()
+                });
+            } else {
+                res.render('pokemon/list', {
+                    title: 'Pokemon List',
+                    data: rows,
+                    loggedInUser: Main.loggedInUser.getJson()
+                });
+            }
+        });
+
+
+    }
+
 }
