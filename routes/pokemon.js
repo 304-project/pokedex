3adbad1fce19434a3c51ab35dc2cd05072cc4bc6
Pahul <pahulpn@gmail.com>
2018-03-28 11:09:28 -0700

    just name processing so far

--- routes/pokemon.js
+++ routes/pokemon.js
@@ -1,17 +1,44 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 var app_1 = require("../app");
-var query = 'select p.pokedexId, p.name, p.height, p.weight, h.identifier, t.typeName from pokemon p join pokemontypes pt on p.pokedexId = pt.pokedexId join habitats h on p.habitatId = h.habitatId join typeslist t on pt.typeId = t.typeId';
+var origQuery = 'select p.pokedexId, p.name, p.height, p.weight, h.identifier, t.typeName from pokemon p join pokemontypes pt on p.pokedexId = pt.pokedexId join habitats h on p.habitatId = h.habitatId join typeslist t on pt.typeId = t.typeId';
+var query = origQuery;
+var sort = ' order by p.pokedexId asc';
 var PokemonRoute = (function () {
     function PokemonRoute() {
     }
     PokemonRoute.get = function (req, res) {
+        req.assert('name', 'Name is required').notEmpty();
+        var errors = req.validationErrors();
+        if (!errors) {
+            var searchPokemon = {
+                name: req.sanitize('name').escape().trim(),
+            };
+            var searchId = {
+                name: req.sanitize('name').escape().trim(),
+            };
+            var searchId = {
+                name: req.sanitize('name').escape().trim(),
+            };
+            var searchId = {
+                name: req.sanitize('name').escape().trim(),
+            };
+            query += " where " + +searchPokemon + "'";
+        }
+        else {
+            query = origQuery;
+        }
         app_1.default.connection.query(query, function (err, rows, fields) {
             if (err) {
                 req.flash('error', err);
                 res.render('pokemon/list', {
                     title: 'Pokemon List',
                     data: '',
+                    id: '',
+                    name: '',
+                    type: '',
+                    habitat: '',
+                    query: '',
                     loggedInUser: app_1.default.loggedInUser.getJson()
                 });
             }
@@ -19,12 +46,15 @@ var PokemonRoute = (function () {
                 res.render('pokemon/list', {
                     title: 'Pokemon List',
                     data: rows,
+                    id: '',
+                    name: '',
+                    type: '',
+                    habitat: '',
+                    query: '',
                     loggedInUser: app_1.default.loggedInUser.getJson()
                 });
             }
         });
-    };
-    PokemonRoute.search = function (req, res) {
     };
     return PokemonRoute;
 }());
--- routes/pokemon.ts
+++ routes/pokemon.ts
@@ -1,32 +1,60 @@
 //pokemon.js
 import Main from '../app';
 import * as express from "express"
-
-const query = 'select p.pokedexId, p.name, p.height, p.weight, h.identifier, t.typeName from pokemon p join pokemontypes pt on p.pokedexId = pt.pokedexId join habitats h on p.habitatId = h.habitatId join typeslist t on pt.typeId = t.typeId';
+const origQuery = 'select p.pokedexId, p.name, p.height, p.weight, h.identifier, t.typeName from pokemon p join pokemontypes pt on p.pokedexId = pt.pokedexId join habitats h on p.habitatId = h.habitatId join typeslist t on pt.typeId = t.typeId';
+var query = origQuery;
+var sort = ' order by p.pokedexId asc';
 
 export class PokemonRoute {
 
     public static get(req: any, res: express.Response) { //, next: express.NextFunction){
+        req.assert('name', 'Name is required').notEmpty();           //Validate name
+        var errors = req.validationErrors();
+
+        if (!errors) {
+            var searchPokemon = {
+                name: req.sanitize('name').escape().trim(),
+            };
+            var searchId = {
+                name: req.sanitize('name').escape().trim(),
+            };
+            var searchId = {
+                name: req.sanitize('name').escape().trim(),
+            };
+            var searchId = {
+                name: req.sanitize('name').escape().trim(),
+            };
+            query += " where " +  + searchPokemon + "'";
+        }
+        else{
+            query = origQuery;
+        }
         Main.connection.query(query, (err: any, rows: any, fields: any) => {
             if (err) {
                 req.flash('error', err);
                 res.render('pokemon/list', {
                     title: 'Pokemon List',
                     data: '',
+                    id: '',
+                    name: '',
+                    type: '',
+                    habitat: '',
+                    query: '',
                     loggedInUser: Main.loggedInUser.getJson()
                 });
             } else {
                 res.render('pokemon/list', {
                     title: 'Pokemon List',
                     data: rows,
+                    id: '',
+                    name: '',
+                    type: '',
+                    habitat: '',
+                    query: '',
                     loggedInUser: Main.loggedInUser.getJson()
                 });
             }
         });
-    }
-
-    public static search(req: any, res: express.Response){
-        
     }
 
 }
